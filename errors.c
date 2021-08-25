2fd79e0e2ea7d77805e67b03572f5eb0c74edd74
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-08-25 18:44:08 -0300

    Update errors.c

--- errors.c
+++ errors.c
@@ -1,27 +1,27 @@
 #include "holberton.h"
 
 /**
- * errors - prints errors based on case
- * @error: error number associated with perror statement
- * Return: void
+ * errors - a
+ * @error: a
+ * Return: a
  */
 void errors(int error)
 {
 	switch (error)
 	{
-	case 1: /* writes fork error to stderr */
+	case 1:
 		write(STDERR_FILENO, ERR_FORK, _strlen(ERR_FORK));
 		perror("Error");
 		break;
 
-	case 2: /* writes execve error to stderr */
+	case 2: 
 		perror("Error");
 		break;
 
-	case 3: /* writes malloc error to stderr */
+	case 3: 
 		write(STDERR_FILENO, ERR_MALLOC, _strlen(ERR_MALLOC));
 		break;
-	case 4: /* writes empty path error to stderr */
+	case 4: 
 		write(STDERR_FILENO, ERR_PATH, _strlen(ERR_PATH));
 		break;
 
