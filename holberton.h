cbbe613f2ca7722274b2d30f4dab33860d9fe913
JuanDraper <85587600+JuanDraper@users.noreply.github.com>
2021-08-25 18:47:03 -0300

    Update holberton.h

--- holberton.h
+++ holberton.h
@@ -12,18 +12,18 @@
 #define BUFFER 1024
 #define TRUE 1
 #define PROMPT "$ "
-/* error strings */
+
 #define ERR_MALLOC "Unable to malloc space\n"
 #define ERR_FORK "Unable to fork and create child process\n"
 #define ERR_PATH "No such file or directory\n"
 extern char **environ;
 
 /**
- * struct list_s - linked list of variables
- * @value: value
- * @next: pointer to next node
+ * struct list_s - a
+ * @value: a
+ * @next:a
  *
- * Description: generic linked list struct for variables.
+ * Description: a
 **/
 typedef struct list_s
 {
@@ -32,12 +32,12 @@ typedef struct list_s
 } list_s;
 
 /**
- * struct built_s - linked list of builtins
- * @name: name of builtin
- * @p: pointer to function
+ * struct built_s - a
+ * @name:a
+ * @p: a
  *
- * Description: struct for builtin functions.
-**/
+ * Description: a
+ */
 typedef struct built_s
 {
 	char *name;
@@ -51,25 +51,21 @@ char *_which(char *command, char *fullpath, char *path);
 int child(char *fullpath, char **tokens);
 void errors(int error);
 
-/* utility functions */
 void _puts(char *str);
 int _strlen(char *s);
 int _strcmp(char *name, char *variable, unsigned int length);
 int _strncmp(char *name, char *variable, unsigned int length);
 char *_strcpy(char *dest, char *src);
 
-/* prototypes for builtins */
 int shell_env(void);
 int shell_exit(void);
 int builtin_execute(char **tokens);
 int shell_num_builtins(built_s builtin[]);
 
-/* prototypes for the helper functions for path linked list */
 char *_getenv(const char *name);
 char **copy_env(char **environ_copy, unsigned int environ_length);
 list_s *pathlist(char *variable, list_s *head);
 
-/* prototypes for free functions */
 void free_all(char **tokens, char *path, char *line, char *fullpath, int flag);
 void free_dp(char **array, unsigned int length);
-#endif /* HOLBERTON_H */
+#endif 
